16ecfefa20c8b170eca2c795ca1da402d9d366e4
Lelzin λ <d3cryptofc@gmail.com>
2026-02-17 04:00:03 -0300

    feat: show plugin banner

--- src/main/java/br/minilambda/FastMud.java
+++ src/main/java/br/minilambda/FastMud.java
@@ -5,11 +5,31 @@ import br.minilambda.listeners.IceBlockFadeToWaterOverDirt;
 import br.minilambda.listeners.PlaceDirtUnderWater;
 import br.minilambda.listeners.WaterBucketOverDirt;
 import br.minilambda.listeners.WaterFlowOverDirt;
+import br.minilambda.utils.AnsiColor;
+import java.util.logging.Logger;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FastMud extends JavaPlugin {
+
+    public void showBanner() {
+        String[] lines = {
+            "  ___  ___  ___ ____",
+            " /__  /__/ /__   /  " + AnsiColor.BLUE + "MUD",
+            "/    /  / ___/  /  v" + this.getDescription().getVersion(),
+            "",
+        };
+
+        Logger logger = this.getLogger();
+        String starting_color;
+
+        for (int idx = 0; idx < lines.length; idx++) {
+            starting_color = idx != 0 ? AnsiColor.DARK_GRAY : AnsiColor.BLUE;
+
+            logger.info(starting_color + lines[idx]);
+        }
+    }
 
     @Override
     public void onEnable() {
@@ -26,5 +46,7 @@ public class FastMud extends JavaPlugin {
         for (Listener listener : listeners) {
             pm.registerEvents(listener, this);
         }
+
+        showBanner();
     }
 }
---
+++ src/main/java/br/minilambda/utils/AnsiColor.java
@@ -0,0 +1,7 @@
+package br.minilambda.utils;
+
+public class AnsiColor {
+
+    public static final String BLUE = "\033[1;36m";
+    public static final String DARK_GRAY = "\033[0;30m";
+}
