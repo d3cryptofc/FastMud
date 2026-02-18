1dc6cf4adf087e570b623fa2ae1fee1b4ac8eced
Lelzin λ <d3cryptofc@gmail.com>
2026-02-18 20:30:57 -0300

    feat(api): create `PluginDefaultMessages.YOU_DONT_HAVE_PERMISSION`

---
+++ src/main/java/com/github/d3cryptofc/fastmud/constant/PluginDefaultMessages.java
@@ -0,0 +1,8 @@
+package com.github.d3cryptofc.fastmud.constant;
+
+public class PluginDefaultMessages {
+
+    public static final String YOU_DONT_HAVE_PERMISSION =
+        PluginPrefixes.CHAT +
+        "§cYou don't have permission to use this command.";
+}
