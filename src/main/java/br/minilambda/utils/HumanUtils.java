a1e6fa3daa594e9c8c54a7b20349e2d5870007c5
Lelzin λ <d3cryptofc@gmail.com>
2026-02-17 22:25:39 -0300

    feat: create `HumanUtils` class to humanize datas

---
+++ src/main/java/br/minilambda/utils/HumanUtils.java
@@ -0,0 +1,13 @@
+package br.minilambda.utils;
+
+public class HumanUtils {
+
+    public static String booleanToHumanLikeOnOff(boolean value) {
+        return String.format(
+            "%s%s%s",
+            value ? AnsiColor.GREEN : AnsiColor.DARK_RED,
+            value ? "on" : "off",
+            AnsiColor.RESET
+        );
+    }
+}
