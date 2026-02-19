e056bbd2a31ee09dc126fbe4dcb774529ab49e7f
Lelzin λ <d3cryptofc@gmail.com>
2026-02-18 21:13:45 -0300

    feat(api): create `CommandGroup` abstract class

---
+++ src/main/java/com/github/d3cryptofc/fastmud/abc/CommandGroup.java
@@ -0,0 +1,29 @@
+package com.github.d3cryptofc.fastmud.abc;
+
+import com.github.d3cryptofc.fastmud.FastMud;
+import java.util.HashMap;
+import java.util.logging.Logger;
+import org.bukkit.command.TabExecutor;
+
+public abstract class CommandGroup implements TabExecutor {
+
+    protected FastMud plugin;
+    protected Logger logger;
+    protected HashMap<String, CommandGroup> subcommands;
+
+    public CommandGroup(FastMud plugin) {
+        this.plugin = plugin;
+        this.logger = plugin.getLogger();
+        this.subcommands = new HashMap<>();
+    }
+
+    public abstract String getName();
+
+    public void addSubcommand(CommandGroup subcommand) {
+        this.subcommands.put(subcommand.getName(), subcommand);
+    }
+
+    public CommandGroup getSubcommand(String name) {
+        return this.subcommands.get(name);
+    }
+}
